2aa0cebca49c0d9d77f895d3bbc2de4267acbab0
harishpallapu <81285240+harishpallapu@users.noreply.github.com>
2025-04-25 02:35:55 -0500

    Delete backend directory

--- backend/models/HeartRate.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const heartRateSchema = new mongoose.Schema({
-  userId: mongoose.Schema.Types.ObjectId,
-  bpm: Number,
-  timestamp: { type: Date, default: Date.now },
-});
-
-export default mongoose.model('HeartRate', heartRateSchema);
--- backend/models/WaterIntake.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const WaterSchema = new mongoose.Schema({
-  userId: String,
-  liters: Number,
-  date: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('Water', WaterSchema);
